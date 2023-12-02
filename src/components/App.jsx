df9125365d82d05bccb52041b49aaf031cf2b6ea
Artemskyba <artemgennadievich@icloud.com>
2023-12-02 12:08:22 +0200

    finished task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
+import { ContactForm } from './form/contacts-form';
+import { Filter } from './contacts/contacts-filter';
+import { ContactList } from './contacts/contacts';
+
 export const App = () => {
   return (
-    <div>
-    </div>);
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </>
+  );
 };
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact(state, action) {
+      const { name: contactName, number: contactNumber } = action.payload;
+      state.push({
+        id: nanoid(),
+        name: contactName,
+        number: contactNumber,
+      });
+    },
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
